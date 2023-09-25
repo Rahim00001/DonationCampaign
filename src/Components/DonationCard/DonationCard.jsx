58850d9d18da3e185392b3565b50c3ecdb5418c0
Rahim00001 <rahimlpt2022@gmail.com>
2023-09-25 11:02:54 +0600

    category wise images added in json fake data

--- src/Components/DonationCard/DonationCard.jsx
+++ src/Components/DonationCard/DonationCard.jsx
@@ -1,7 +1,8 @@
 const DonationCard = ({ donation }) => {
+    const { picture, title, category, card_bg, category_bg, text_bg } = donation
     console.log(donation);
     return (
-        <div>
+        <div className="h-80">
         </div>
     );
 };
