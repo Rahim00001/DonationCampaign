5707a5add5f80a6664e6e7c40fc75bbdd9806c17
Rahim00001 <rahimlpt2022@gmail.com>
2023-09-25 12:41:05 +0600

    responsive banner section added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,19 +1,17 @@
-
+import './Banner.css'
 const Banner = () => {
     return (
-        <div className="hero" style={{ backgroundImage: 'url(https://i.ibb.co/W2GmRjW/Clothing.png)' }}>
-            <div className="hero-overlay bg-opacity-90"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+        <div className="banner relative text-center w-full mx-auto h-[480px] ml-7 md:m-auto">
+            <img src="https://i.ibb.co/271ftgy/Rectangle-4287.png" alt="" />
+            <div className="overlay flex flex-col justify-center items-center w-full">
+                <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl mb-5 lg:mb-10 font-bold'>I Grow By Helping People In Need</h2>
+                <div>
+                    <input className='py-4 pl-5 pr-16 lg:pr-48  rounded-l-lg' type="text" placeholder='Search here.... ' />
+                    <button className='bg-[#FF444A] p-4 rounded-r-lg text-white'>Search</button>
                 </div>
             </div>
         </div>
-        // <div className="overflow-hidden bg-center bg-cover bg-no-repeat opacity-10 h-[70vh]" style={{ backgroundImage: 'url(https://i.ibb.co/W2GmRjW/Clothing.png)' }}>
-        //     <h2 className="text-5xl text-center py-5 my-4">This is banner</h2>
-        // </div >
+
     );
 };
 
