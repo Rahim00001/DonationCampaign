fc95913ff2f38192269259ba40ef8ebeabd99e9f
Rahim00001 <rahimlpt2022@gmail.com>
2023-09-25 11:17:18 +0600

    all donation card displayed dynamicaly

--- src/Components/DonationCard/DonationCard.jsx
+++ src/Components/DonationCard/DonationCard.jsx
@@ -2,7 +2,11 @@ const DonationCard = ({ donation }) => {
     const { picture, title, category, card_bg, category_bg, text_bg } = donation
     console.log(donation);
     return (
-        <div className="h-80">
+        <div className="h-72 w-[300px] rounded-lg " style={{ backgroundColor: card_bg }}>
+            <img src={picture} alt="" />
+            <p className=" w-fit p-3 py-1 text-sm font-medium rounded-lg m-4" style={{ color: "black", backgroundColor: category_bg }}>{category}</p>
+            <h2 style={{ color: text_bg }}>{title}</h2>
+
         </div>
     );
 };
