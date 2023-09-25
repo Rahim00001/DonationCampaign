56f01cdb1c683e965411fae2e6973ae55f1454d9
Rahim00001 <rahimlpt2022@gmail.com>
2023-09-25 15:33:18 +0600

    donate button added

--- src/Components/DonationDetiles/DonationDetiles.jsx
+++ src/Components/DonationDetiles/DonationDetiles.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import './DonationButton.css'
 
 const DonationDetiles = () => {
     const [detiles, setDetiles] = useState([])
@@ -13,7 +14,12 @@ const DonationDetiles = () => {
     return (
         <div className="w-[90%] mx-auto">
             <div>
-                <img className="w-full  my-8 rounded-lg" src={detiles.picture} alt="" />
+                <div className="relative">
+                    <img className="w-full  my-8 rounded-lg" src={detiles.picture} alt="" />
+                    <div className="overlay overlay_1">
+                        <button className="btn absolute -mt-6 -ml-4 border-none text-white font-semibold" style={{ backgroundColor: detiles.text_bg }}>Donate ${detiles.price}</button>
+                    </div>
+                </div>
             </div>
             <h2 className="text-[#0B0B0B] text-4xl font-bold mb-3">{detiles.title}</h2>
             <p className="text-[rgba(11, 11, 11, 0.70)] leading-7 mb-5">{detiles.description}</p>
