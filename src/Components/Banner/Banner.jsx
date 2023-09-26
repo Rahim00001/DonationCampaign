12267e7c9829f3831cd8e2af9be4d191ff21ff2e
Rahim00001 <rahimlpt2022@gmail.com>
2023-09-26 06:26:51 +0600

    error page customized

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div className="banner relative text-center w-full mx-auto h-[480px] ml-7 md:m-auto">
-            <img src="https://i.ibb.co/271ftgy/Rectangle-4287.png" alt="" />
+            <img src="https://i.ibb.co/tPz3tK9/Rectangle-4281.png" alt="" />
             <div className="overlay flex flex-col justify-center items-center w-full">
                 <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl mb-5 lg:mb-10 font-bold'>I Grow By Helping People In Need</h2>
                 <div>
--- src/Components/Error/ErrorPage.jsx
+++ src/Components/Error/ErrorPage.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
         <div className="h-screen flex flex-col gap-3 justify-center items-center">
-            <h2 className="text-7xl">Oops...</h2>
-            <p className="text-2xl">An error occerred.</p>
+            <h2 className="text-9xl">404</h2>
+            <p className="text-2xl mb-5">Sorry, we cant find that page! Dont worry though,everything is STILL AWESOME!</p>
+            <Link to={'/'}><button className="btn bg-red-600 text-white">Go Back Home</button></Link>
         </div>
     );
 };
