449a3a8fa42977b92d9eb8090f7f5e6e4c9bad5f
Rahim00001 <rahimlpt2022@gmail.com>
2023-09-26 07:36:05 +0600

    see all button added

--- src/Components/DonationList/DonationList.jsx
+++ src/Components/DonationList/DonationList.jsx
@@ -5,7 +5,12 @@ import ListCard from "../ListCard/ListCard";
 
 const DonationList = () => {
     const donations = useLoaderData();
-    const [donetion, setDonation] = useState([])
+    const [donetion, setDonation] = useState([]);
+    const [show, setShow] = useState(false);
+    const hideButton = document.getElementById('btn');
+    if (show) {
+        hideButton.classList.add('hidden')
+    }
     useEffect(() => {
         const storedDonationIds = getStoredDonations();
         if (donations.length > 0) {
@@ -14,15 +19,28 @@ const DonationList = () => {
             // console.log(donations, storedDonationIds, listDonation);
             // console.log(listDonation);
         }
-    }, [])
+    }, []);
+    console.log(show);
     return (
         <div>
             <h2 className="text-3xl text-center font-semibold">Total donation: {donetion.length}</h2>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 w-[80%] lg:w-[90%] mx-auto">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 w-[80%] lg:w-[90%] mx-auto my-5">
                 {
-                    donetion.map(donate => <ListCard key={donate.id} donate={donate}></ListCard>)
+                    show ? donetion.map(donate => <ListCard key={donate.id} donate={donate}></ListCard>)
+
+                        : donetion.slice(0, 4).map(donate => <ListCard key={donate.id} donate={donate}></ListCard>)
                 }
             </div>
+            <div>
+                {donetion.length > 4 && (
+                    <div className="flex justify-center">
+                        <button id="btn" onClick={() => setShow(!show)} className="px-5 py-2 bg-[#009444] rounded text-white font-semibold">See All</button>
+                    </div>
+                )}
+            </div>
+            {/* <div className="flex justify-center">
+                <button className="px-5 py-2 bg-[#009444] rounded text-white font-semibold">See All</button>
+            </div> */}
         </div>
     );
 };
